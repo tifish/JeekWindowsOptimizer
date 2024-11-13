cf5bb8ba58dc12fea2d87b81bee029876c2c06f8
Tinyfish <tifish@gmail.com>
2024-11-13 21:18:53 +0800

    Fix Get-AppxPackage error in Windows 10.

--- Tools/MicrosoftStore.cs
+++ Tools/MicrosoftStore.cs
@@ -1,18 +1,36 @@
 ﻿using System.Management.Automation;
 using System.Management.Automation.Runspaces;
+using JeekTools;
+using Microsoft.Extensions.Logging;
+using ZLogger;
 
 namespace JeekWindowsOptimizer;
 
 public static class MicrosoftStore
 {
+    private static readonly ILogger Log = LogManager.CreateLogger<MainViewModel>();
+
     private static readonly PowerShell _powerShell = PowerShell.Create();
 
     static MicrosoftStore()
     {
-        _powerShell.AddCommand("Set-ExecutionPolicy")
-            .AddParameter("Scope", "Process")
-            .AddParameter("ExecutionPolicy", "Bypass")
-            .Invoke();
+        try
+        {
+            _powerShell.AddCommand("Set-ExecutionPolicy")
+                .AddParameter("Scope", "Process")
+                .AddParameter("ExecutionPolicy", "Bypass")
+                .Invoke();
+
+            _powerShell.Commands.Clear();
+            _powerShell.AddCommand("Import-Module")
+                .AddParameter("Name", "AppX")
+                .AddParameter("UseWindowsPowerShell")
+                .Invoke();
+        }
+        catch (Exception e)
+        {
+            Log.ZLogError(e, $"Failed to set execution policy");
+        }
     }
 
     private static Command GetPackageCommand(string packageName) =>
@@ -27,9 +45,17 @@ public static class MicrosoftStore
 
     public static bool HasPackage(string packageName)
     {
-        _powerShell.Commands.Clear();
-        _powerShell.Commands.AddCommand(GetPackageCommand(packageName));
-        return _powerShell.Invoke().Count > 0;
+        try
+        {
+            _powerShell.Commands.Clear();
+            _powerShell.Commands.AddCommand(GetPackageCommand(packageName));
+            return _powerShell.Invoke().Count > 0;
+        }
+        catch (Exception e)
+        {
+            Log.ZLogError(e, $"Failed to check if package {packageName} exists");
+            return false;
+        }
     }
 
     public static async Task UninstallPackage(string packageName)
