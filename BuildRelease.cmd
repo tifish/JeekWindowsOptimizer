282312b85676954a7f3dc5013719e92c790f74ef
Tinyfish <tifish@gmail.com>
2024-11-24 22:16:31 +0800

    Remove Logs directory before build.

--- BuildRelease.cmd
+++ BuildRelease.cmd
@@ -2,7 +2,8 @@
 setlocal
 cd /d "%~dp0"
 
-del /q "bin\*.deps.json" "bin\*.runtimeconfig.json" "bin\Libs\*" "bin\Logs"
+del /q "bin\*.deps.json" "bin\*.runtimeconfig.json" "bin\Libs\*"
+rd /s /q "bin\Logs"
 
 "C:\Program Files\Microsoft Visual Studio\2022\Preview\MSBuild\Current\Bin\amd64\MSBuild.exe" JeekWindowsOptimizer.sln -t:Rebuild -p:Configuration=Release
 @REM dotnet build JeekWindowsOptimizer.sln -c Release
