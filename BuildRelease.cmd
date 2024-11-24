4e2147f25c4716db4b25294b6906543c3872b689
Tinyfish <tifish@gmail.com>
2024-11-24 22:18:31 +0800

    Pause when building error.

--- BuildRelease.cmd
+++ BuildRelease.cmd
@@ -6,6 +6,7 @@ del /q "bin\*.deps.json" "bin\*.runtimeconfig.json" "bin\Libs\*"
 rd /s /q "bin\Logs"
 
 "C:\Program Files\Microsoft Visual Studio\2022\Preview\MSBuild\Current\Bin\amd64\MSBuild.exe" JeekWindowsOptimizer.sln -t:Rebuild -p:Configuration=Release
+if errorlevel 1 pause
 @REM dotnet build JeekWindowsOptimizer.sln -c Release
 
 endlocal
