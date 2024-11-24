58c2b850915f4eb5f4d92a4a336a114f076f8bf1
Tinyfish <tifish@gmail.com>
2024-11-24 22:15:39 +0800

    Update build script to build with C# 13.

--- BuildRelease.cmd
+++ BuildRelease.cmd
@@ -2,7 +2,9 @@
 setlocal
 cd /d "%~dp0"
 
-del /q "bin\*.deps.json" "bin\*.runtimeconfig.json" "bin\Libs\*"
-dotnet build JeekWindowsOptimizer.sln -c Release
+del /q "bin\*.deps.json" "bin\*.runtimeconfig.json" "bin\Libs\*" "bin\Logs"
+
+"C:\Program Files\Microsoft Visual Studio\2022\Preview\MSBuild\Current\Bin\amd64\MSBuild.exe" JeekWindowsOptimizer.sln -t:Rebuild -p:Configuration=Release
+@REM dotnet build JeekWindowsOptimizer.sln -c Release
 
 endlocal
