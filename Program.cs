91b043ffb2615b58c4e70ca7dfe8e1758ff5951f
Tinyfish <tifish@gmail.com>
2024-11-29 01:00:27 +0800

    Catch global exception.

--- Program.cs
+++ Program.cs
@@ -1,26 +1,37 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using JeekTools;
+using Microsoft.Extensions.Logging;
+using ZLogger;
 
 namespace JeekWindowsOptimizer;
 
 sealed class Program
 {
+    private static readonly ILogger Log = LogManager.CreateLogger<MainViewModel>();
+
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
     public static void Main(string[] args)
     {
-        var appDirectory = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
-        if (appDirectory != null)
-            Environment.CurrentDirectory = appDirectory;
+        try
+        {
+            var appDirectory = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            if (appDirectory != null)
+                Environment.CurrentDirectory = appDirectory;
 
-        if (Design.IsDesignMode)
-            LogManager.DisableLogging();
+            if (Design.IsDesignMode)
+                LogManager.DisableLogging();
 
-        BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+            BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception ex)
+        {
+            Log.ZLogCritical(ex, $"An error occurred in Main");
+        }
     }
 
     // Avalonia configuration, don't remove; also used by visual designer.
