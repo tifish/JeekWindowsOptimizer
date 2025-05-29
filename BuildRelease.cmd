4a9114708263d4ef8b188df1df14b30f9056ce79
Tinyfish <tifish@gmail.com>
2025-05-29 11:12:51 +0800

    Find msbuild.exe in variant VS 2022 versions.

--- BuildRelease.cmd
+++ BuildRelease.cmd
@@ -1,11 +1,23 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 cd /d "%~dp0"
 
-del /q "bin\*.deps.json" "bin\*.runtimeconfig.json" "bin\Libs\*"
-rd /s /q "bin\Logs"
+del /q "bin\*.deps.json" "bin\*.runtimeconfig.json" "bin\Libs\*" 2>nul
+rd /s /q "bin\Logs" 2>nul
 
-"C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\amd64\MSBuild.exe" JeekWindowsOptimizer.sln -t:Rebuild -p:Configuration=Release
+for %%v in (Preview Enterprise Professional Community) do (
+    set msbuild="C:\Program Files\Microsoft Visual Studio\2022\%%v\MSBuild\Current\Bin\amd64\MSBuild.exe"
+    if exist "!msbuild!" (
+        goto :build
+    )
+)
+
+echo No MSBuild found
+pause
+exit /b
+
+:build
+%msbuild% JeekWindowsOptimizer.sln -t:Rebuild -p:Configuration=Release
 if errorlevel 1 pause
 
 endlocal
