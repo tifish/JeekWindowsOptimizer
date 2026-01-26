aa2f8d9bf7a686c3a9bffa10ac4ad1d3ca45d633
Tinyfish <tifish@gmail.com>
2026-01-26 11:12:35 +0800

    Use dotnet to build the project to avoid warning.

--- BuildRelease.cmd
+++ BuildRelease.cmd
@@ -5,19 +5,7 @@ cd /d "%~dp0"
 del /q "bin\*.deps.json" "bin\*.runtimeconfig.json" "bin\Libs\*" 2>nul
 rd /s /q "bin\Logs" 2>nul
 
-for %%v in (Preview Enterprise Professional Community) do (
-    set msbuild="C:\Program Files\Microsoft Visual Studio\2022\%%v\MSBuild\Current\Bin\amd64\MSBuild.exe"
-    if exist "!msbuild!" (
-        goto :build
-    )
-)
-
-echo No MSBuild found
-pause
-exit /b
-
-:build
-%msbuild% JeekWindowsOptimizer.sln -t:Rebuild -p:Configuration=Release
+dotnet build --configuration Release JeekWindowsOptimizer.sln
 if errorlevel 1 pause
 
 endlocal
