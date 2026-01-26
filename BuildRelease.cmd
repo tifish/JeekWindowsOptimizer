ce13ba018743164fae7d2a173e5a179c2e628dbe
Tinyfish <tifish@gmail.com>
2026-01-26 11:33:15 +0800

    Optimize startup speed using R2R.

--- BuildRelease.cmd
+++ BuildRelease.cmd
@@ -5,7 +5,7 @@ cd /d "%~dp0"
 del /q "bin\*.deps.json" "bin\*.runtimeconfig.json" "bin\Libs\*" 2>nul
 rd /s /q "bin\Logs" 2>nul
 
-dotnet build --configuration Release JeekWindowsOptimizer.sln
+dotnet publish --configuration Release JeekWindowsOptimizer.sln
 if errorlevel 1 pause
 
 endlocal
